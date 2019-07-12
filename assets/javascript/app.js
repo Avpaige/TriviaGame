c88f243afb290c05948ee1fabafbf484dd0db62d
Ari Virgil-Paige <avpaige@gmail.com>
2019-07-11 22:13:58 -0700

    changed from loop to random question selection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -70,7 +70,7 @@ $(document).ready(function(){
             answer: "d",
             right: "Correct! Friday After Next was released in 2002!",
             wrong: "Wrong! Friday After Next is the correct answer!",
-           },
+        },
 
            {  prompt: "In Mean Girls, what color do the Plastics, wear on Wednesdays?",
     
@@ -82,55 +82,55 @@ $(document).ready(function(){
             answer: "c",
             right: "Correct! Get in loser, we're going shopping!",
             wrong: "Wrong! On Wednesday's we wear pink! Stop trying to make fetch happen",
-           },
+        },
 
-        //    {  prompt: "",
+           {  prompt: "What is the name of the Head Elf in The Santa Clause?",
     
-        //     a: "",
-        //     b: "",
-        //     c: "",
-        //     d: "",
+            a: "Brody",
+            b: "Bernard",
+            c: "Bryan",
+            d: "Benjamin",
 
-        //     answer: ,
-        //     right: "",
-        //     wrong: "",
-        //    },
+            answer: "b",
+            right: "Correct! You might as well put on the suit!",
+            wrong: "Wrong! You clear have never seen Bernard's sweater game!",
+        },
 
-        //    {  prompt: "",
+           {  prompt: "Which of the following is NOT a Bruce Willis film?",
     
-        //     a: "",
-        //     b: "",
-        //     c: "",
-        //     d: "",
+            a: "16 Blocks",
+            b: "Catch .44 ",
+            c: "The Fifth Element",
+            d: "3 Days to Kill",
 
-        //     answer: ,
-        //     right: "",
-        //     wrong: "",
-        //    },
+            answer: "d",
+            right: "Correct! 3 Days to Kill IS a movie but Bruce Willis isn't in it!",
+            wrong: "Wrong! Bruce Willis has been in a lot of movies, but not 3 Days to Kill!",
+        },
 
-        //    {  prompt: "",
+           {  prompt: "What is the name of the ficitional town where Sixteen Candles, Pretty in Pink, Weird Science and The Breakfast Club all take place? ",
     
-        //     a: "",
-        //     b: "",
-        //     c: "",
-        //     d: "",
+            a: "Shermer, IL",
+            b: "Herman, IL",
+            c: "Hershey, IL",
+            d: "Sanford, IL",
 
-        //     answer: ,
-        //     right: "",
-        //     wrong: "",
-        //    },
+            answer: "a",
+            right: "Correct! You know better than to head to Shermer!",
+            wrong: "Wrong! Shermer, IL is the correct answer!",
+        },
 
-        //    {  prompt: "",
+           {  prompt: "Not including the latest Creed movies, how many films are in the Rocky franchise?",
     
-        //     a: "",
-        //     b: "",
-        //     c: "",
-        //     d: "",
+            a: "2",
+            b: "3",
+            c: "4",
+            d: "5",
 
-        //     answer: ,
-        //     right: "",
-        //     wrong: "",
-        //    },
+            answer: "d",
+            right: "Correct! Adrian, knows she can count on you!",
+            wrong: "Wrong! There are 5 films in the Rocky franchise! That's a lot of punchses!",
+        },
         ]
     //press start to start game
     //create thirty second interval per question
@@ -146,33 +146,33 @@ $(document).ready(function(){
    
 
 var start = function (){
-    for (var i=0; i< questions.length; i++){
-        $("#question").html(questions[i].prompt);
-        $("#a").html(questions[i].a);
-        $("#b").html(questions[i].b);
-        $("#c").html(questions[i].c);
-        $("#d").html(questions[i].d);
-
+    var randomQuestionIndex = Math.floor( Math.random() * questions.length);
+    var chosenQuestion = questions[randomQuestionIndex]
+    $("#question").html(chosenQuestion.prompt);
+    $("#a").html(chosenQuestion.a);
+    $("#b").html(chosenQuestion.b);
+    $("#c").html(chosenQuestion.c);
+    $("#d").html(chosenQuestion.d);
+}
     $(".btn").on("click", function (){
     var response = $(this).attr("data-value");
 
-        if (response === questions[i].answer){
+    if (response === chosenQuestion.answer){
             correct++; 
 
-console.log(questions[i].answer)
+console.log(this)
 console.log("this is answer value after click")
 
-            $("#question").html(questions[i].right);
-        } else if (response != questions[i].answer){
+            $("#question").html(chosenQuestion.right);
+        } else if (response != chosenQuestion.answer){
             incorrect++;
-            $("#question").html(questions[i].wrong);
+            $("#question").html(chosenQuestion.wrong);
         } else (response === false) 
             unaswered++;
 
         });
 
-        }
-    }
+        
 console.log ("correct is : " + correct)
 console.log ("correct is : " + incorrect)
 console.log ("unansered is :" + unaswered)
